613f81f9d099993e3064ad801ce80e1cc82ada3a
Nitya <nityapandey2004@gmail.com>
2025-12-24 15:39:05 +0530

    [Nitya Pandey]: added a program Anagram

---
+++ gcr-codebase/Extras/StringHandling/AnagramCheck.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        // Convert to lowercase
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // Check length
+        if (str1.length() != str2.length()) {
+            System.out.println("Strings are NOT Anagrams");
+            return;
+        }
+
+        // Convert to char array
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        // Sort arrays
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare arrays
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("Strings are Anagrams");
+        } else {
+            System.out.println("Strings are NOT Anagrams");
+        }
+    }
+}
