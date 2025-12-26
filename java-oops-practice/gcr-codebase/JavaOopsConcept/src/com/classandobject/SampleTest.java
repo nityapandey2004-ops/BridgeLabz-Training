cd562deb1c96b8e305db2a1f1c596b8ce7ed5c6e
Nitya <nityapandey2004@gmail.com>
2025-12-26 14:27:49 +0530

    [Nitya]: added initial setup

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/classandobject/SampleTest.java
@@ -0,0 +1,10 @@
+package com.classandobject;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("My first program in Eclipse");
+	}
+
+}
