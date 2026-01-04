bfd05d900d72a6487f4069f07d3c9cb1e3e1e5c4
Nitya <nityapandey2004@gmail.com>
2026-01-05 01:21:23 +0530

    [Nitya]:added a program of Animal

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritence/level1/Animal.java
@@ -0,0 +1,16 @@
+package com.inheritence.level1;
+
+public class Animal {
+	
+		private String name;
+		private int age;
+		
+		public Animal(String name, int age){
+			this.name = name;
+			this.age = age;
+		}
+		public void makeSound() {
+			System.out.println("");
+		}
+	
+}
