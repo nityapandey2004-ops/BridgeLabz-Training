722b3eae3c331c2cdc83129ea8ebbdd577989404
Nitya <nityapandey2004@gmail.com>
2026-01-05 02:04:39 +0530

    [Nitya]:added a program of SavingAccount.java

---
+++ java-oops-practice/scenario-based/javaoopspracticescenariobased/src/com/bankmanagementsystem/SavingAccount.java
@@ -0,0 +1,21 @@
+package com.bankmanagementsystem;
+
+public class SavingAccount extends Account
+{
+	private double interestRate;
+
+	public SavingAccount(double balance, double interestRate)
+	{
+		super(balance);
+		this.interestRate = interestRate;
+	}
+
+	public void calculateInterest()
+	{
+		double interest = balance * interestRate / 100;
+		balance += interest;
+		System.out.println("Interest Added: ₹" + interest);
+	}
+}
+
+
