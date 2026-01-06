13f5a8fd4e4742eb8aeb11e253e7fa950a0698d1
Nitya <nityapandey2004@gmail.com>
2026-01-06 12:33:56 +0530

    [Nitya]: added program of linkrd list

--- dsa-practice/gcr-codebase/javaDSApractice/src/com/linkedlist/doublylinkedlist/undoandredo/TextEditorMain.java
+++ dsa-practice/gcr-codebase/javaDSApractice/src/com/linkedlist/doublylinkedlist/undoandredo/TextEditorMain.java
@@ -1,5 +1,27 @@
 package com.linkedlist.doublylinkedlist.undoandredo;
 
 public class TextEditorMain {
+	public static void main(String[] args) {
+
+        TextEditor editor = new TextEditor();
+
+        editor.addState("Hello");
+        editor.addState("Hello World");
+        editor.addState("Hello World!");
+        editor.displayCurrentState();
+        editor.undo();
+        editor.displayCurrentState();
+
+        editor.undo();
+        editor.displayCurrentState();
+
+        editor.redo();
+        editor.displayCurrentState();
+        editor.addState("Hello Java");
+        editor.displayCurrentState();
+
+        editor.redo(); 
+    }
+
 
 }
