027df51bbb1525e0c9e9b3370ed72767e9e8dd3f
Nitya <nityapandey2004@gmail.com>
2026-02-04 16:29:05 +0530

    [Nitya Pandey] override

---
+++ java-collection-practice/java-collection-practice/javaCollectionPractice/src/annotations/override/Dog.java
@@ -0,0 +1,8 @@
+package annotations.override;
+public class Dog extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
---
+++ java-collection-practice/java-collection-practice/javaCollectionPractice/src/annotations/override/OverrideTest.java
@@ -0,0 +1,8 @@
+package annotations.override;
+public class OverrideTest {
+
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.makeSound();
+    }
+}
